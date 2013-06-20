c07d3b72541a488714eb1a2264d7602514d85837
Matt Revell <mattrevell82@me.com>
2013-06-20 21:00:33 +0100

    Testing CI.

---
+++ GuildBrowserLogicTests/CharactersTests.h
@@ -0,0 +1,13 @@
+//
+//  CharactersTests.h
+//  GuildBrowser
+//
+//  Created by Matt Revell on 20/06/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharactersTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTest.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTest.h
+//  GuildBrowser
+//
+//  Created by Matt Revell on 20/06/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTest : SenTestCase
+
+@end
